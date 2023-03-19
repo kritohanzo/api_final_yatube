67e51b460ad294c928fd374107d866c66de3671d
Вагиф Алиев <kritohanzo0@gmail.com>
2023-03-19 18:19:41 +0500

    follow not working

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -4,6 +4,11 @@ from django.db import models
 User = get_user_model()
 
 
+class Group(models.Model):
+    title = models.CharField(max_length=64)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
@@ -11,10 +16,10 @@ class Post(models.Model):
         User, on_delete=models.CASCADE, related_name='posts')
     image = models.ImageField(
         upload_to='posts/', null=True, blank=True)
+    group = models.ForeignKey(Group, on_delete=models.SET_NULL, related_name='posts', null=True, blank=True)
 
     def __str__(self):
         return self.text
-
 
 class Comment(models.Model):
     author = models.ForeignKey(
@@ -24,3 +29,8 @@ class Comment(models.Model):
     text = models.TextField()
     created = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True)
+    
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='following')
+    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followers')
